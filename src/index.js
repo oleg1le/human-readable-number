84a46edba5d17f1cc216ee955ff283a6e5f53419
oleg1le <oleg1le@gmail.com>
2022-02-23 13:56:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable (number) {
-  let a = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  let a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
   let b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
 
     if ((number = number.toString()).length > 9) return 'overflow'; 
